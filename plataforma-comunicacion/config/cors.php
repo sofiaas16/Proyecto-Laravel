e55f23cd9c09908010b30fb056ba1274038fb998
Danil0-1 <danilomuskus1@gmail.com>
2025-09-25 22:35:09 +0000

    feat: fix register.html

--- plataforma-comunicacion/config/cors.php
+++ plataforma-comunicacion/config/cors.php
@@ -3,7 +3,12 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://127.0.0.1:8002'], // tu frontend
+    'allowed_origins' => [
+        'http://127.0.0.1:8080', // tu frontend
+        'http://127.0.0.1:8000', // tu backend (por si haces pruebas directas)
+        'http://localhost:8080',
+        'http://localhost:8000',
+    ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => ['Authorization'],
