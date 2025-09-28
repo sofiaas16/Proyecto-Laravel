56060d4e2f3e7c40efeb70904108597bebb9726e
Danil0-1 <danilomuskus1@gmail.com>
2025-09-28 12:19:59 +0000

    feat: :sparkles: redirecciones del web.php cambiadas

--- plataforma-comunicacion/routes/web.php
+++ plataforma-comunicacion/routes/web.php
@@ -3,10 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return redirect('/login.html'); // Página inicial -> login.html
+    return redirect('/src/views/login.html'); // Página inicial -> login.html
 });
 
 // Usamos ".html" en la URL, pero sigue devolviendo una vista Blade
-Route::view('/register.html', 'auth.register')->name('register');
-Route::view('/login.html', 'auth.login')->name('login');
-Route::view('/home.html', 'auth.home')->name('home');
+Route::view('/src/views/register.html', 'auth.register')->name('register');
+Route::view('/src/views/login.html', 'auth.login')->name('login');
+Route::view('/src/views/home.html', 'auth.home')->name('home');
